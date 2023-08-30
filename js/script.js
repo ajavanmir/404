bc68aa022867121a8eb2502f018a6e3679c787ab
amir javanmir <74532873+ajavanmir@users.noreply.github.com>
2023-08-30 07:48:40 +0330

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const parent = document.querySelector("#container");
+window.onmousemove = function(e){
+    let x = e.clientX / 5;
+    let y = e.clientY / 5;
+
+    parent.style.backgroundPositionX = x + "px";
+    parent.style.backgroundPositionY = y + "px";
+}
